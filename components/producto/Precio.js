4d811813ffbb03adae6444d68e2a1e72f507c84d
Johan <johan.garcia165@gmail.com>
2024-04-04 12:34:19 -0400

    Actulizacion Luis

--- components/producto/Precio.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Precio(props) {
-  const { producto } = props;
-
-  const precio = producto?.ppa_precio.toFixed(2);
-  return <div className="font-bold">$ {precio} </div>;
-}
--- context/AuthContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-const AuthContext = createContext({
-  auth: undefined,
-  login: () => null,
-  logout: () => null,
-  SetReloadUser: () => null,
-});
-export default AuthContext;
