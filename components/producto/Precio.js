12c7f6d0b4704be2fb5e233d80b62963718b99b7
Luis <luisaparicioaldana@gmail.com>
2024-05-12 07:08:34 -0400

    buscador

--- components/producto/Precio.js
+++ components/producto/Precio.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Precio(props) {
   const { producto } = props;
+  console.log(producto, "valr")
 
-  const precio = producto?.ppa_precio.toFixed(2);
+  const precio = producto?.ppa_precio?.toFixed(2);
   return <div className="font-bold">$ {precio} </div>;
 }
--- components/productos/RecommendablesRow.js
+++ components/productos/RecommendablesRow.js
@@ -1,12 +1,69 @@
 import React, { useState } from "react";
 import { TableCell, TableRow } from "@mui/material";
 import styled from "@emotion/styled";
+import Marca from "../producto/Marca";
+import Precio from "../producto/Precio";
+import ProductoInfo from "../producto/ProductoInfo";
+import Contador from "../producto/Contador";
 
 export default function RecommendablesRow(props) {
 const { eq } = props
 
+const TableRowStyled = styled(TableRow)`
+&:nth-of-type(odd) {
+  background-color: #e8e8ff;
+}
+&:nth-of-type(even) {
+  background-color: #f5f5f5;
+}
+& td,
+th {
+  width: fit-content;
+  text-align: center;
+}
+`;
+
   return (
-    <div className="flex flex-row gap-8">
+
+
+
+    <TableRowStyled
+     // key={producto?.pre_id}
+      className="text-black p-5 flex justify-between w-full last-of-type:rounded-b-lg items-center"
+    >
+      <TableCell className="w-full">
+         <ProductoInfo producto={eq} /> 
+      </TableCell>
+      <TableCell className="w-full text-center">
+       {/*  {producto?.aplicaciones ? (
+          <Aplicaciones
+            aplicaciones={producto.aplicaciones}
+            srubro={producto.rubro}
+          />
+        ) : null} */}
+      </TableCell>
+      <TableCell className="w-full flex justify-center text-center">
+        <Marca producto={eq} />
+      </TableCell>
+      <TableCell className="w-full text-center">
+        <Precio producto={eq} />
+      </TableCell>
+      {/* contador */}
+       <TableCell colSpan={3} className="w-full flex justify-center">
+        <div className="w-full flex justify-center">
+          <Contador
+            producto={eq}
+           /*  quantity={quantity}
+            setQuantity={setQuantity}
+            handleAddItem={handleAddItem}
+            handleRemoveItem={handleRemoveItem}
+            addItemToCart={addItemToCart} */
+          />
+        </div>
+      </TableCell> 
+     
+    </TableRowStyled>
+   /*  <div className="flex flex-row gap-8">
         <text>
             Imagen producto
         </text>
@@ -22,6 +79,6 @@ const { eq } = props
   </text>
 
 
-    </div>
+    </div> */
   )
 }
